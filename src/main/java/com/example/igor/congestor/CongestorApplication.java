de9a4f16e965f52a06abb6b541540347ed29255f
igorsalgado <salgado.igor.is@gmail.com>
2022-06-16 14:03:59 -0300

    Inicializando o projeto

---
+++ src/main/java/com/example/igor/congestor/CongestorApplication.java
@@ -0,0 +1,13 @@
+package com.example.igor.congestor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CongestorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CongestorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/igor/congestor/CongestorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.igor.congestor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CongestorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
